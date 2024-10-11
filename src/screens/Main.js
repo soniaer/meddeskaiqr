5182cce937afdf4fdfa1d78dd98f6e5d7ddb16cd
soniaer <soniaer987@gmail.com>
2024-10-11 19:13:28 +0530

    Update Main.js

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,5 +1,7 @@
 import '../App.css';
 
+
+
 function Main() {
   return (
     <div
@@ -14,7 +16,7 @@ function Main() {
       }}
     >
  <div style={{height:"100%", width: "100%",overflowX:"hidden" }}>
-        <div style={{ width: "60%",
+        <div style={{ width: "94%",
           maxHeight:'100px',height:"100%",marginLeft:"3%",
           backgroundColor:"transparent",
           display:"flex",flexDirection:"column",justifyContent:"space-between"
@@ -22,22 +24,29 @@ function Main() {
           alignItems:"flex-start",
           color:"#fff",fontSize:"28px",paddingTop:"2%"
          }}>
-          <div style={{display:"flex",width:"100%", }}>
+          <div style={{display:"flex",width:"100%",justifyContent:"space-between", }}>
 
         
-          <div style={{width:"50%",backgroundColor:"#fff",justifyContent:"center",
-        display:"flex",padding:"1%",maxHeight:"70px"}}>
+          <div style={{width:"60%",backgroundColor:"#fff",justifyContent:"space-between",
+        display:"flex",padding:"1%",maxHeight:"70px",}}>
 
          
-     <img alt="" src={require('../img/med.png')} style={{width:"85%",height:"26px"}} />
+     <img alt="" src={require('../img/med.png')} style={{width:"95%",height:"26px"}} />
      </div>
      <div style={{ width: "55%",border:"3px solid #9ea3ac", 
           maxHeight:'40px',height:"100%",
           justifyContent:"center",textAlign:"center",
-          alignItems:"center",color:"#fff",fontSize:"70%",display:"flex",fontWeight:"600",
+          alignItems:"center",color:"#fff",fontSize:"60%",display:"flex",fontWeight:"600",
           overflow:"hidden",
-          marginLeft:"25%"}}>
+          marginLeft:"15%"}}>
 Barcode / QR Data Viewer</div>
+<div onClick={()=>{document.getElementById("myModal").style.display="block"}} style={{ width: "30%",
+          maxHeight:'40px',height:"100%",
+          justifyContent:"center",textAlign:"center",
+          alignItems:"center",color:"#000",fontSize:"70%",display:"flex",fontWeight:"500",
+          overflow:"hidden",
+          marginLeft:"25%",backgroundColor:"#fff",cursor:"pointer"}}>
+ADD</div>
      </div><span style={{fontSize:"70%"}}>It Just works Better</span>
           </div>
         <div style={{alignItems:"center", width: "94%",
@@ -52,7 +61,6 @@ Barcode / QR Data Viewer</div>
         maxHeight:'40px',height:"100%",marginLeft:"0%",marginTop:"0%",
         justifyContent:"flex-start", paddingLeft:"1%",
         alignItems:"center",display:"flex",color:"#fff",fontSize:"18px",
-       
        }}>
 Title
        </div>
@@ -121,9 +129,88 @@ be construed as real data or representative of any actual real medical data .
 
 This webpage is been powered by MedDesk-AI all rights received ©
 </div>
+<div id="myModal" class="modal">
+    
+        <div class="modal-content">
+          <span class="close" onClick={()=>{document.getElementById("myModal").style.display="none"}}>&times;</span>
+       
+
+         
+          <h4 style={{color:"#156082"}}>Add Data</h4>
+          <div style={{display:"flex",justifyContent:"center",alignItems:"center",width:"100%"}}>
+
+         
+          <div style={{width:"80%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
+        flexDirection:"column"}}>
+          <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
+        }}>
+
+          
+          <label>Title:</label>
+  
+   <input style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
+   </input>
+   </div>
+   <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
+        }}>
+   <label>Manufacture:</label>
+  
+   <input style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
+   </input>
+   </div>
+   <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
+        }}>
+   <label>Weight:</label>
+  
+  <input style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
+  </input>
+  </div>
+  <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
+        }}>
+  <label>Description:</label>
+  
+  <input style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
+  </input>
+  </div>
+  <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
+        }}>
+  <label>Barcode:</label>
+  
+  <input style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
+  </input>
+  </div>
+  <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
+        }}>
+  <label>Datasheet:</label>
+  
+  <input style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
+  </input>
+  </div>
+  <div style={{width:"100%",paddingTop:"1%",paddingBottom:"1%",color:"#156082",display:"flex",justifyContent:"space-between",
+        }}>
+  <label>Product Picture:</label>
+  
+  <input style={{width:"60%",border:"2px solid #156082",marginBottom:"2%"}}>
+  </input>
+  </div>
+
+   </div>
+   
+   </div>
+   <button style={{border:"2px solid #156082",width:"40%",backgroundColor:"#156082",height:"30px",
+  cursor:"pointer"}}>
+
+
+<span style={{color:"white"}} onClick={()=>{document.getElementById("myModal").style.display="none"}}>
+ Add</span>
+ </button>
+        </div>
+      </div>
 </div>
 </div>
+
     </div>
+ 
   );
 }
 
